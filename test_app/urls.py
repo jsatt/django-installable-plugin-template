ce5c7b5116eaf43e346fcda43fb5f0c125804fc9
Jeremy Satterfield <jsatt22@gmail.com>
2013-05-10 10:24:23 -0500

    don't include app urls by default, may not be needed, fixes #3

--- test_app/urls.py
+++ test_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^{{project_name}}/',
-        include('{{project_name}}.urls', '{{project_name}}')),
+    #url(r'^{{project_name}}/',
+    #    include('{{project_name}}.urls', '{{project_name}}')),
 )
 urlpatterns += staticfiles_urlpatterns()
